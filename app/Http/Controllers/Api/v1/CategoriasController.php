2cfa571986fc066f31e7d1fb5a0db5f2469fc30e
jose diaz <jose7312000@gmail.com>
2022-03-08 20:07:52 -0500

    Configuracion de carpetas para la v1 de la api

---
+++ app/Http/Controllers/Api/v1/CategoriasController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Api\v1\Categorias;
+use Illuminate\Http\Request;
+
+class CategoriasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Api\v1\Categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categorias $categorias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Api\v1\Categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categorias $categorias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Api\v1\Categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categorias $categorias)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/v1/ComentariosController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Api\v1\Comentarios;
+use Illuminate\Http\Request;
+
+class ComentariosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Api\v1\Comentarios  $comentarios
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comentarios $comentarios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Api\v1\Comentarios  $comentarios
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comentarios $comentarios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Api\v1\Comentarios  $comentarios
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comentarios $comentarios)
+    {
+        //
+    }
+}
